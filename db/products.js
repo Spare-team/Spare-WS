097ba3c56ffb385f0bdbabaea750bf9a603b50a7
Ricardo <dev.rsierra@gmail.com>
2014-03-30 00:36:15 -0600

    db Producto

--- db/products.js
+++ db/products.js
@@ -1,4 +1,23 @@
+{
+	"_id" : ObjectId("531668fcce5fb5df2ff89a9a"),
+	"model" : "XWDWS",
+	"brand" : "SONY",
+	"description" : "television",
+	"cat" : "Electronics",
+	"subcat" : "TV",
+	"_ranks" : [ ],
+	"images" : [
+		"61evuTKNXjL._SL1500_.jpg",
+		"71uso7FdLvL._SL1500_.jpg",
+		"617AvgSF9BL._SL1500_.jpg"
+	],
+	"__v" : 0
+}
+/*
+
+
 use local;
+
 var products = [
 	{model: 'VXFD55', brand: 'SONY', description: 'TV LED 55"', cat: 'Electronics', subcat: 'TV'},
 	{model: 'OGT35', brand: 'LG', description: 'TV Plasma 46"', cat: 'Electronics', subcat: 'TV'}
@@ -18,4 +37,4 @@ use local;
 db.createCollection("Users")
 var adress={country:'Mexico',statet:''city:''};
 db.Users.insert({userName:'Rios',passWord:"111",email:'Rios@Spare.com'})
-
+*/
